c78a1754703ecb6957a30c6ca7833dda93d5fb31
Oliver Cordes <ocordes@astro.uni-bonn.de>
2018-09-11 19:36:30 +0200

    - add #3185, #4083 bricks

--- pyldd/bricks.py
+++ pyldd/bricks.py
@@ -172,6 +172,7 @@ known_bricks = { 2356:  [ '2356', None ],     # brick 4 x 6
                  3069:  [ '3069', None ],     # flat tile 1 x 2
                  3062:  [ '3062', None ],     # brick 1 x 1 round open stud
                  3070:  [ '3070', None ],     # flat tile 1 x 1
+                 3185:  [ '3185', None ],     # fence 1 x 4 x 2
                  3245:  [ '3245', None ],     # brick 1 x 2 x 2
                  3460:  [ '3460', None ],     # plate 1 x 8
                  3470:  [ '3470', None ],     # plant tree oval 4 x 4 x 6
@@ -185,6 +186,7 @@ known_bricks = { 2356:  [ '2356', None ],     # brick 4 x 6
                  3940:  [ '3940', None ],     # space stand 2 x 2 x 2
                  3941:  [ '3941', None ],     # brick 2 x 2 round
                  3942:  [ '3942', None ],     # cone 2 x 2 x 2 open stud
+                 4083:  [ '4083', None ],     # bar 1 x 4 x 2
                  4162:  [ '4162', None ],     # flat tile 1 x 8
                  4287:  [ '4287', None ],     # slope brick 33 3 x 1 inverted
                  4477:  [ '4477', None ],     # plate 1 x 10
